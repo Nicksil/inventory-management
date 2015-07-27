d5a75070183b87d24be1f62d7fd0f0760bb16034
Nick Silvester <nicksil@gmail.com>
2015-07-27 16:39:28 -0400

    Added basic user authentication

--- inventory_management/characters/views.py
+++ inventory_management/characters/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as _login
+from django.contrib.auth import logout as _logout
+from django.shortcuts import redirect
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                _login(request, user)
+                return redirect('index')
+            else:
+                # Handle disabled account w/loggin
+                pass
+        else:
+            # Handle invalid login w/logging
+            pass
+    return render(request, 'characters/login.html')
+
+
+def logout(request):
+    _logout(request)
+    return redirect('index')
--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -1,6 +1,14 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+from characters.views import login
+from characters.views import logout
 
 urlpatterns = [
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index')
 ]
