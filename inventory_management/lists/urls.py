31f445a291f36f01d60689edf54d365c76736257
Nick Silvester <nicksil@gmail.com>
2015-07-27 23:28:17 -0400

    Added shopping list

--- inventory_management/lists/urls.py
+++ inventory_management/lists/urls.py
@@ -3,7 +3,11 @@ from __future__ import absolute_import
 from django.conf.urls import url
 
 from .views import shoppinglist_create_view
+from .views import shoppinglist_list_view
+from .views import shoppinglist_update_view
 
 urlpatterns = [
     url(r'^create/$', shoppinglist_create_view, name='shoppinglist_create'),
+    url(r'^list/$', shoppinglist_list_view, name='shoppinglist_list'),
+    url(r'^(?P<pk>\d+)/update/$', shoppinglist_update_view, name='shoppinglist_update'),
 ]
--- inventory_management/lists/views.py
+++ inventory_management/lists/views.py
@@ -1,5 +1,62 @@
+from __future__ import absolute_import
+
+from django.shortcuts import redirect
 from django.shortcuts import render
+
+from .models import ShoppingList
+from characters.models import Character
+from items.models import Item
+
+
+def shoppinglist_list_view(request):
+    shoppinglists = ShoppingList.objects.all()
+
+    return render(
+        request,
+        'lists/shoppinglist_list_view.html',
+        {'shoppinglists': shoppinglists}
+    )
+
+
+def shoppinglist_update_view(request, pk):
+    shoppinglist = ShoppingList.objects.get(pk=pk)
+    if request.method == 'POST':
+        name = request.POST['name']
+        items = request.POST['items']
+
+        shoppinglist.name = name
+        shoppinglist.save()
+
+        if len(items):
+            item_obj = Item.objects.get(type_name=items)
+            shoppinglist.items.add(item_obj)
+
+        return redirect('lists:shoppinglist_list')
+
+    return render(
+        request,
+        'lists/shoppinglist_update_view.html',
+        {'shoppinglist': shoppinglist}
+    )
 
 
 def shoppinglist_create_view(request):
-    return render(request, 'lists/shoppinglist_create_view.html')
+    if request.method == 'POST':
+        character_obj = Character.objects.get(name=request.POST['character'])
+        name = request.POST['name']
+        item_obj = Item.objects.get(type_name=request.POST['items'])
+
+        shoppinglist = ShoppingList.objects.create(
+            name=name,
+            character=character_obj,
+        )
+        shoppinglist.items.add(item_obj)
+
+        return redirect('lists:shoppinglist_create')
+
+    characters = request.user.characters.all()
+    return render(
+        request,
+        'lists/shoppinglist_create_view.html',
+        {'characters': characters}
+    )
