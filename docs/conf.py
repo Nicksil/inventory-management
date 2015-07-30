404ca7122dbcb600e4eca1bfc2d2e30570457d84
Nick Silvester <nicksil@gmail.com>
2015-07-30 19:51:17 -0400

    Changed theme for sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
