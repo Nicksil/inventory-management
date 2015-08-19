3720e02571eca65cb3875945d3245e20c674d8ae
Nick Silvester <nicksil@gmail.com>
2015-08-19 14:26:50 -0400

    Added test for new implementation

---
+++ inventory_manager/monitoring/tests/test_utils.py
@@ -0,0 +1,58 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from model_mommy import mommy
+
+from characters.models import Order
+
+
+class TestMonitoringUtils(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_attribs = mommy.prepare('User')
+        cls.user = User.objects.create_user(
+            username=cls.user_attribs.username,
+            password=cls.user_attribs.password)
+
+        cls.char = mommy.make('Character', user=cls.user)
+        cls.order_1 = mommy.make(
+            'Order',
+            character=cls.char,
+            vol_remaining=10,
+            qty_threshold=15)
+        cls.order_2 = mommy.make(
+            'Order',
+            character=cls.char,
+            vol_remaining=20,
+            qty_threshold=15)
+
+    def test_check_qty_threshold(self):
+        self.client.login(
+            username=self.user.username, password=self.user_attribs.password)
+
+        uri = reverse('monitoring:check_qty_threshold', kwargs={'pk': self.char.pk})
+        response = self.client.get(uri, follow=True)
+
+        message_text = 'The following items have met their quantity threshold: {}'.format(
+            self.order_1.item.type_name)
+        self.assertContains(response, message_text)
+
+        expected_redirect_uri = reverse('characters:order_list', kwargs={'pk': self.char.pk})
+        self.assertRedirects(response, expected_redirect_uri)
+
+        # Now check as if no orders have met threshold
+        update_order = Order.objects.get(pk=self.order_1.pk)
+        update_order.vol_remaining = 20
+        update_order.save()
+
+        uri = reverse('monitoring:check_qty_threshold', kwargs={'pk': self.char.pk})
+        response = self.client.get(uri, follow=True)
+
+        message_text = 'The following items have met their quantity threshold: {}'.format(
+            self.order_1.item.type_name)
+        self.assertNotContains(response, message_text)
+
+        expected_redirect_uri = reverse('characters:order_list', kwargs={'pk': self.char.pk})
+        self.assertRedirects(response, expected_redirect_uri)
--- inventory_manager/monitoring/utils.py
+++ inventory_manager/monitoring/utils.py
@@ -8,23 +8,23 @@ from characters.models import Character
 
 
 # http://stackoverflow.com/a/434328/1770233
-def chunker(seq, size):
-    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+# def chunker(seq, size):
+#     return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
 
-def is_outbid(order):
-    order_price = order.price
+# def is_outbid(order):
+#     order_price = order.price
 
     # Try to get last sell price for item,
     # if not price exists for item yet, return
     # None... for now
-    try:
-        last_item_price = order.item.prices.last().sell
-    except AttributeError:
-        print('NO SELL PRICE FOUND FOR {}'.format(order.item.type_name))
-        return
+    # try:
+    #     last_item_price = order.item.prices.last().sell
+    # except AttributeError:
+    #     print('NO SELL PRICE FOUND FOR {}'.format(order.item.type_name))
+    #     return
 
-    return order_price > last_item_price
+    # return order_price > last_item_price
 
 
 def qty_threshold_met_alert(char):
